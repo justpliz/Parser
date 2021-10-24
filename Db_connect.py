a9a7a5f9a8599e80cea0cceba9d275d733d30204
Абай Молдабеков <abay9508@gmail.com>
2021-10-25 03:16:15 +0600

    Переделал бд по ТЗ, заполняется таблица items кроме времени

--- Db_connect.py
+++ Db_connect.py
@@ -6,36 +6,44 @@ try:
                                          database='parser',
                                          user='root',
                                          password='MYSQL')
-    # Получаем кортеж ссылок для парсинга, выбираем нужную в формате string
+
     cursor = connection.cursor()
-    cursor.execute("SELECT URL FROM input_URL")
+    cursor.execute("SELECT RESOURCE_URL FROM resource")
     originalURL = cursor.fetchall()[0]
-    originalStrURL = ' '.join(map(str, originalURL))
+    originalStrURL = ''.join(map(str, originalURL))
     #print(originalStrURL)
     #connection.close()
 
-    # Создание таблицы ссылок для парсинга
-    mySql_Create_Table_Query = """CREATE TABLE input_URL (
-                                 Id int(11) NOT NULL,
-                                 URL varchar(250) NOT NULL,
-                                 PRIMARY KEY (Id)) """
+
+    mySql_Create_Table_Query = """CREATE TABLE resource (
+                                 RESOURCE_ID bigint(20) NOT NULL,
+                                 RESOURCE_NAME varchar(255) NULL,
+                                 RESOURCE_URL varchar(255) NULL,
+                                 top_tag varchar(255) NOT NULL,
+                                 bottom_tag varchar(255) NOT NULL,
+                                 title_tag varchar(255) NOT NULL,
+                                 date_cut varchar(255) NOT NULL,
+                                 PRIMARY KEY (RESOURCE_ID)) """
 
     cursor = connection.cursor()
     result = cursor.execute(mySql_Create_Table_Query)
-    print("input_URL Table created successfully ")
+    print("resource Table created successfully ")
 
-    # Создание таблицы с выходными данными
-    mySql_Create_Table_Query = """CREATE TABLE output_content ( 
-                                 Id int(11) NOT NULL,
-                                 URL varchar(250) NOT NULL,
-                                 Heading varchar(500) NOT NULL,
-                                 Data DATE NOT NULL,
-                                 Content TEXT NOT NULL,                                               
-                                 PRIMARY KEY (Id)) """
+
+    mySql_Create_Table_Query = """CREATE TABLE items ( 
+                                 id int(11) NOT NULL,
+                                 res_id int(11) NOT NULL,
+                                 link varchar(255) NOT NULL,
+                                 title text NOT NULL,
+                                 content text NOT NULL,
+                                 nd_date int(11) NOT NULL,
+                                 s_date int(11) NOT NULL,
+                                 not_date date NOT NULL,              
+                                 PRIMARY KEY (id)) """
 
     cursor = connection.cursor()
     result = cursor.execute(mySql_Create_Table_Query)
-    print("output_content Table created successfully ")
+    print("items Table created successfully ")
 
 except mysql.connector.Error as error:
     print("Failed to create table in MySQL: {}".format(error))
@@ -45,3 +53,28 @@ finally:
         connection.close()
         print("MySQL connection is closed")
 
+
+
+def insert_varibles_into_table(id, res_id, link, title, content, nd_date, s_date, not_date):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='parser',
+                                             user='root',
+                                             password='MYSQL')
+        cursor = connection.cursor()
+        MySql_Insert_Items_Query = """INSERT INTO Items (id, res_id, link, title, content, nd_date, s_date, not_date)
+                               VALUES
+                               (%s, %s, %s, %s, %s, %s, %s, %s) """
+        record = (id, res_id, link, title, content, nd_date, s_date, not_date)
+        cursor.execute(MySql_Insert_Items_Query, record)
+        connection.commit()
+        print("Record inserted successfully into Laptop table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
--- Parse_news.py
+++ Parse_news.py
@@ -1,7 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+from dateparser import parse
 import urllib.request
+
+
 from Db_connect import originalStrURL
+from Db_connect import insert_varibles_into_table
 
 URL = originalStrURL
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) '
@@ -19,29 +23,44 @@ def get_content(html):
         news.append({
             'title': item.find('h3', class_='preview-title preview-title--medium').get_text(strip=True),
             'link': item.find('a', class_='article-preview-mixed').get('href'),
-            'data': item.find('time', class_='preview-info-item-secondary').get_text(strip=True),
+            'nd_date': item.find('time', class_='preview-info-item-secondary').get_text(strip=True),
         })
-        print(news)
 
         response = urllib.request.urlopen(news[0]['link'])
         linksURL = response.read()
         soup = BeautifulSoup(linksURL, 'html.parser')
-        newsContent = soup.find_all('article', class_='article')
-        print(newsContent)
+        items = soup.find_all('article', class_='article')
+        outputContent = []
+        for item in items:
+             outputContent.append({
+                'content': item.find_all('p', class_='align-left formatted-body__paragraph'),
+                'image': item.find_all('img', class_='inline-picture'),
+             })
 
-        return news #вернет один
+        news += outputContent
+        return news
 
+# def blabla(self, nd_date):
+#     self.nd_date = nd_date
+#     parse(nd_date)
 
 
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
         news = get_content(html.text)
+
+        link = ''.join(map(str, news[0]['link']))
+        title = ''.join(map(str, news[0]['title']))
+        content = ''.join(map(str, news[1]['content']))
+        nd_date = ''.join(map(str, news[0]['nd_date']))
+        # nd_date = nd_date
+        # parse(nd_date)
+        # blabla(nd_date)
+        #date_int(nd_date)
+        insert_varibles_into_table(1, 1, link, title, content, nd_date, nd_date, nd_date)
     else:
         print('Error')
 
 parse()
 
-
-def URL():
-    return None
